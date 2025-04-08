576afc74812637a6d23ddca94892590beaedd13c
pawsengineer <manovisut.ktp@gmail.com>
2025-04-08 20:10:37 +0700

    fix(grinder): rw locked while grinding

--- src/bin/sui-address-grinder.rs
+++ src/bin/sui-address-grinder.rs
@@ -1,16 +1,11 @@
 use clap::Parser;
 use sui_address_grinder::{args::GrindArgs, grinder::Grinder};
 
-pub fn main() {
+#[tokio::main]
+pub async fn main() {
     let args = GrindArgs::parse();
     args.must_validate();
 
     let grinder = Grinder::new();
-    let (addr, sp) = grinder.grind(args).unwrap();
-
-    println!();
-    println!("====================================================");
-    println!("Address:\t{}", addr);
-    println!("Seedphase:\t{}", sp);
-    println!("====================================================");
+    grinder.grind(args);
 }
